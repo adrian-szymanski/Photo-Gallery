d03d92fae2975f86858a43dcb1b06fbdda0c2f0f
Adrian Szymanski <adrian.szymanski89@gmail.com>
2018-04-13 16:02:11 +0200

    Assets
    Minor assets and description changes.

--- src/app/image/shared/image.service.ts
+++ src/app/image/shared/image.service.ts
@@ -14,14 +14,14 @@ export class ImageService{
 }
 
 const IMAGES = [
-    {"id": 1, "category": "boats", "caption": "View from a boats", "url": "assets/img/1.JPG"},
-    {"id": 2, "category": "boats", "caption": "View from a boats", "url": "assets/img/2.JPG"},
-    {"id": 3, "category": "boats", "caption": "View from a boaty", "url": "assets/img/3.JPG"},
-    {"id": 4, "category": "camping", "caption": "View from a boate", "url": "assets/img/4.JPG"},
-    {"id": 5, "category": "camping", "caption": "View from a boatq", "url": "assets/img/5.JPG"},
-    {"id": 6, "category": "camping", "caption": "View from a boatu", "url": "assets/img/6.JPG"},
-    {"id": 7, "category": "library", "caption": "View from a boati", "url": "assets/img/7.JPG"},
-    {"id": 8, "category": "library", "caption": "View from a boato", "url": "assets/img/8.JPG"},
-    {"id": 9, "category": "library", "caption": "View from a boatj", "url": "assets/img/9.JPG"},
-    {"id": 10, "category": "library", "caption": "View from a boata", "url": "assets/img/10.JPG"}
+    {"id": 1, "category": "boats", "caption": "Random view 1", "url": "assets/img/1.JPG"},
+    {"id": 2, "category": "boats", "caption": "Random view 1", "url": "assets/img/2.JPG"},
+    {"id": 3, "category": "boats", "caption": "Random view 1", "url": "assets/img/3.JPG"},
+    {"id": 4, "category": "camping", "caption": "Random view 2", "url": "assets/img/4.JPG"},
+    {"id": 5, "category": "camping", "caption": "Random view 2", "url": "assets/img/5.JPG"},
+    {"id": 6, "category": "camping", "caption": "Random view 2", "url": "assets/img/6.JPG"},
+    {"id": 7, "category": "library", "caption": "Random view 3", "url": "assets/img/7.JPG"},
+    {"id": 8, "category": "library", "caption": "Random view 3", "url": "assets/img/8.JPG"},
+    {"id": 9, "category": "library", "caption": "Random view 3", "url": "assets/img/9.JPG"},
+    {"id": 10, "category": "library", "caption": "Random view 3", "url": "assets/img/10.JPG"}
 ]
